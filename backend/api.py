c25df270c626a2abae48a0010d97ec5533836971
PeterKahenya <kahenya0@gmail.com>
2022-03-13 21:57:30 +0300

    Added mongodb database

--- backend/api.py
+++ backend/api.py
@@ -1,80 +1,61 @@
+from asyncio import all_tasks
 from flask import Flask,request
 from flask_restful import Resource, Api
-import pymongo
+from pymongo import MongoClient,ReturnDocument
+from bson import json_util,ObjectId
+import json
 
 app = Flask(__name__)
 api = Api(app)
+client = MongoClient('localhost', 27017)
+db = client["jets"]
+tasks = db["tasks"] #Tasks Collection
 
-tasks_list = [
-    {
-        "id":1,
-        "name":"Emailing Task",
-        "description":"Build RPA Workflow for Auto Emailing Users",
-        "start_time":"08:00 am",
-        "end_time":"08:30 am",
-    },
-    {
-        "id":2,
-        "name":"Update Computations",
-        "description":"Update the report with the latest data and findings",
-        "start_time":"09:00 am",
-        "end_time":"10:30 am",
-    },
-    {
-        "id":3,
-        "name":"Call Boss regarding new potential",
-        "description":"Call director to report the conversation on the elevator",
-        "start_time":"11:00 am",
-        "end_time":"11:40 am",
-    },
-    {
-        "id":4,
-        "name":"Audit X Client",
-        "description":"Send initial reqiests for information on the upcoming audit",
-        "start_time":"12:00 pm",
-        "end_time":"01:30 pm",
-    },
-    {
-        "id":5,
-        "name":"Have Lunch",
-        "description":"Enjoy some lunch with Team members",
-        "start_time":"01:30 pm",
-        "end_time":"2:30 pm",
-    }
-]
 
 class TasksList(Resource):
     def get(self):
-        return tasks_list
+        all_tasks = tasks.find({})
+        all_taks_json = [];
+        for t in all_tasks:
+            print(t)
+            all_taks_json.append(json.loads(json.dumps(t,default=str)))
+        return all_taks_json,200
 
     def post(self):
         data = request.get_json()
-        data["id"] = len(tasks_list)+1
-        tasks_list.append(data)
-        return data
+        tasks.insert_one(data)
+        return json.loads(json.dumps(data,default=str)),201
 
 
 class SingleTask(Resource):
 
     def get_or_404(self,id):
-        task = [task for task in tasks_list if task["id"]==int(id)]
+        task = tasks.find_one({"_id":ObjectId(id)})
         if task:
-            return task[0]
+            return json.loads(json.dumps(task,default=str))
         else:
-            return {"message":"404 Not Found"}
+            raise Exception("404 Not Found")
 
     def get(self,id):
-        return self.get_or_404(id)
+        try:
+            return self.get_or_404(id)
+        except Exception as e:
+            return {"message":str(e)},404 
 
     def put(self,id):
-        data = request.get_json()
-        for k,v in data.items():
-            tasks_list[int(id)-1][k]=v
-        return tasks_list[int(id)-1]
+        try:
+            data = request.get_json()
+            task = tasks.find_one_and_update({"_id":ObjectId(id)},{"$set":data},return_document=ReturnDocument.AFTER)
+            return json.loads(json.dumps(task,default=str)),204
+        except Exception as e:
+            return {"message":str(e)},400 
 
     def delete(self,id):
-        tasks_list.pop(int(id)-1)
-        return {"message":"401 Removed"}
+        task = tasks.find_one_and_delete({"_id":ObjectId(id)})
+        if task:
+            return json.loads(json.dumps(task,default=str)),200
+        else:
+            return {"message":"Object not found!"},404 
 
 
 api.add_resource(TasksList, '/tasks/')
